6b4cad3f0ffeaba8521fa59657b268079f79a4d5
Julianne Crawford <julianne.marik@gmail.com>
2018-11-08 14:00:53 -0600

    routes returning all filtered commits and pull requests

--- server/api/repos.js
+++ server/api/repos.js
@@ -1,29 +1,37 @@
 const router = require('express').Router()
 module.exports = router
 const axios = require('axios')
+const octokit = require('@octokit/rest')()
 
 // CLIENT ID AND SECRET
 const clientId = process.env.GITHUB_CLIENT_ID
 const clientSecret = process.env.GITHUB_CLIENT_SECRET
 
 // COMMITS FROM A REPOSITORY
-router.get('/:owner/:repo/commits', async(req, res, next) => {
+router.get('/:owner/:repo/commits', async (req, res, next) => {
   try {
     const owner = req.params.owner
     const repo = req.params.repo
     const url = `https://api.github.com/repos/${owner}/${repo}/commits?per_page=100`
 
-    const {data: commits} = await axios.get(`${url}&&client_id=${clientId}&&client_secret=${clientSecret}`)
+    let response = await axios.get(
+      `${url}&&client_id=${clientId}&&client_secret=${clientSecret}`
+    )
+    let {data} = response
+    while (octokit.hasNextPage(response)) {
+      response = await octokit.getNextPage(response)
+      data = data.concat(response.data)
+    }
 
-      const filteredCommits = []
-      commits.forEach(commit => {
-      if(commit.commit.committer.name !== 'GitHub'){
+    const filteredCommits = []
+    data.forEach(commit => {
+      if (commit.commit.committer.name !== 'GitHub') {
         filteredCommits.push({
           message: commit.commit.message,
           date: commit.commit.committer.date,
-          commiterName: commit.commit.committer.name,
-          commiterEmail: commit.commit.committer.email,
-          authorAvatarUrl: commit.committer.avatar_url
+          userName: commit.commit.committer.name,
+          userEmail: commit.commit.committer.email,
+          userAvatarUrl: commit.committer.avatar_url
         })
       }
     })
@@ -35,14 +43,32 @@ router.get('/:owner/:repo/commits', async(req, res, next) => {
 })
 
 // PULL REQUESTS FROM A REPOSITORY
-router.get('/:owner/:repo/pulls', async(req, res, next) => {
+router.get('/:owner/:repo/pulls', async (req, res, next) => {
   try {
     const owner = req.params.owner
     const repo = req.params.repo
     const url = `https://api.github.com/repos/${owner}/${repo}/pulls?state=all`
 
-    const {data: pullRequests} = await axios.get(`${url}&&client_id=${clientId}&&client_secret=${clientSecret}`)
-    res.send(pullRequests)
+    let response = await axios.get(
+      `${url}&&client_id=${clientId}&&client_secret=${clientSecret}`
+    )
+    let {data} = response
+    while (octokit.hasNextPage(response)) {
+      response = await octokit.getNextPage(response)
+      data = data.concat(response.data)
+    }
+
+    const filteredPulls = []
+    data.forEach((pull) => {
+      filteredPulls.push({
+        title: pull.title,
+        userAvatarUrl: pull.user.avatar_url,
+        dataCreated: pull.created_at,
+        dateClosed: pull.closed_at
+      })
+    })
+
+    res.send(filteredPulls)
   } catch (err) {
     next(err)
   }
