28d81660f737d130317646f793a3aac5590ddd40
Julianne Crawford <julianne.marik@gmail.com>
2018-11-08 14:12:51 -0600

    get commits added to repos store

--- client/store/repos.js
+++ client/store/repos.js
@@ -1,14 +1,39 @@
 import axios from 'axios'
 
 // ACTION TYPES
+const GET_COMMITS = 'GET_COMMITS'
 
 // INITIAL STATE
+const initialState = {
+  commits: [],
+  pulls: []
+}
 
 // ACTION CREATORS
+const getCommits = commits => ({type: GET_COMMITS, commits})
 
 // THUNK CREATORS
+export const fetchCommits = (owner, repo) => async dispatch => {
+  try {
+    const {data: commits} = await axios.get(`/${owner}/${repo}/commits`)
+    dispatch(getCommits(commits))
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 // HANDLERS
-
+const handlers = {
+  [GET_COMMITS]: (state, action) => {
+    return {...state, commits: action.commits}
+  }
+}
 // REDUCER
+export default function (state = initialState, action) {
+  if (!handlers.hasOwnProperty(action.type)) {
+    return state
+  } else {
+    return handlers[action.type](state, action)
+  }
+}
 
